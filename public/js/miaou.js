b906e656dd04b09909f0828ac6180353130189f0
Gary <garypegeot@gmail.com>
2015-01-18 17:49:26 +0100

    NodeJS improved version, fixes: #2, #4, #5, #6, #7, #8, #9, #13

--- public/js/miaou.js
+++ public/js/miaou.js
@@ -13,27 +13,84 @@
  *  @license   GNU
  */
 
-(function () {	
+(function () {
 	var app = angular.module('miaou', []);
-	
-	app.controller('GlobalController', ['$scope', '$http', function($scope, $http) {
-			
+
+	app.controller('KeywordsController', ['$scope', '$http', function ($scope, $http) {
 		/*************************
 		 * Variables definition  *
 		 ************************/
 		var searchEngines = ['google', 'bing'],
-		validUrl = /^(https?:\/\/)/,
-		strToArray = function (str, sep) {
-			return str.split(sep).map(function(text) {
-				return text.trim();
-			});
-		};
-		
+				validUrl = /^(https?:\/\/)/,
+				strToArray = function (str, sep) {
+					return str.split(sep).map(function (text) {
+						return text.trim();
+					});
+				},
+				analyzeUrl = function (url) {
+					var data = {
+						url: url,
+						strict: $scope.searchParams.strict,
+						keywords: strToArray($scope.searchParams.keywords, ','),
+						maxResults: $scope.searchParams.maxResults
+					};
+
+					$http.post('/keyword', data).success(function (response) {
+						$scope.waiting = false;
+						if (!response.success)
+							return;
+						var results = {
+							origin: data.url,
+							active: true,
+							data: []
+						}
+						response.p.forEach(function (text) {
+							if ($scope.keywordsList.indexOf(text) !== -1)
+								return false;
+							$scope.keywordsList.push(text);
+							results.data.push(text);
+						});
+						if (results.data.length)
+							$scope.results.push(results);							
+					}).error(function () {
+						$scope.waiting = false;
+					});
+				},
+				removeTextFromResults = function (text) {
+					var index = $scope.keywordsList.indexOf(text);
+					
+					if (index === -1)
+						return false;
+					
+					$scope.keywordsList.splice(index, 1);
+					return true;
+				};
+
 		$scope.linkList = [];
 		$scope.keywordsList = [];
-		$scope.emptyLinks = function() {
+		$scope.results = [];
+
+		$scope.emptyLinks = function () {
 			$scope.linkList.length = 0;
+			$scope.keywordsList.length = 0;
+			$scope.results.length = 0;
+			$scope.searchParams.keywords = '';
 		}
+		
+		$scope.removeResult = function (index) {
+			var resultRemoved = $scope.results.splice(index, 1)[0];
+			resultRemoved.data.forEach(function(text){
+				removeTextFromResults(text);
+			});
+		}
+		
+		$scope.removeText = function (index, result) {
+			var text = result.data.splice(index, 1)[0];
+			removeTextFromResults(text);
+			if (!result.data.lentgh)
+				$scope.removeResult($scope.results.indexOf(result));
+		}
+		
 		$scope.waiting = false;
 		$scope.searchParams = {
 			keywords: '',
@@ -41,52 +98,28 @@
 			strict: 0,
 			maxResults: 2
 		};
-		
-		$scope.fetchLinks = function() {
+
+		$scope.fetchKeywords = function () {
 			$scope.waiting = true;
 			searchEngines.forEach(function (searchEngine) {
 				var data = {
 					searchType: $scope.searchParams.searchType,
 					keywords: strToArray($scope.searchParams.keywords, ',')
 				};
-				
-				$http.post('/links/' + searchEngine, data).success(function(response) {
+
+				$http.post('/links/' + searchEngine, data).success(function (response) {
 					$scope.waiting = false;
-					response.urls.forEach(function(url) {
-						if (validUrl.test(url) && $scope.linkList.indexOf(url) === -1)
-							$scope.linkList.push(url);						
+					response.urls.forEach(function (url) {
+						if (!validUrl.test(url) || $scope.linkList.indexOf(url) !== -1)
+							return false;
+						
+						$scope.linkList.push(url);
+						analyzeUrl(url);
 					});
 				}).error(function () {
 					$scope.waiting = false;
 				});
 			});
 		};
-		
-		$scope.fetchKeywords = function () {
-			$scope.waiting = true;
-			$scope.linkList.forEach(function (url) {
-				var data = {
-					url: url,
-					strict: $scope.searchParams.strict,
-					keywords: strToArray($scope.searchParams.keywords, ','),
-					maxResults: $scope.searchParams.maxResults
-				};
-				
-				$http.post('/keyword', data).success(function(response) {
-					$scope.waiting = false;
-					if (!response.success)
-						return;
-					
-					response.p.forEach(function(text) {
-						if ($scope.keywordsList.indexOf(text) === -1)
-							$scope.keywordsList.push(text);						
-					});
-				}).error(function () {
-					$scope.waiting = false;
-				});
-			});
-		};
-	}]);	
+	}]);
 })();
-
-
